ef2c367343db7106c21b103dac3fc9c0ad0b4be4
Kristjan Kõiv <kristjankv32@gmail.com>
2022-12-06 07:05:02 +0200

    Added day 6

---
+++ 2022/solutions/day6.py
@@ -0,0 +1,86 @@
+from main import *
+import random, math
+# import matplotlib.pyplot as plt
+import shelve
+
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
+UPPER_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUMBERS = "0123456789"
+
+
+def solve1(data):
+
+    # first = map_lines(data[:1], [int], ",")
+    parsed = map_lines(data, [str])
+    # parsed = map_lines(data, [int])
+    # parsed = my_map(parsed, lambda x: [int(a) for a in x])
+    # parsed = my_map(parsed, lambda x: (x[0].split(","), x[2].split(",")))
+    # parsed = my_map(parsed, lambda x: [(a[0].split(","), a[1].split(",")) for a in x])
+    parsed = my_map(parsed, lambda x: x)
+
+    # SOLUTION
+    i = 0
+    s = ""
+    d = {}
+    u = set()
+    l = []
+    ans = 0
+    count = 0
+    buffer = []
+
+    for ch in data[0]:
+        count += 1
+        buffer.append(ch)
+        if len(buffer) == len(set(buffer)) and len(buffer) == 4:
+            return count
+        if len(buffer) == 4:
+            buffer = buffer[1:]
+
+
+    # END SOLUTION
+
+    my_shelf = shelve.open("shelf.tmp",'n')
+    for key in dir():
+        try:
+            my_shelf[key] = locals()[key]
+        except:
+            print(f"ERROR shelving {key}")
+    my_shelf.close()
+    return ans
+
+
+def solve2(data):
+    my_shelf = shelve.open("shelf.tmp")
+    for key in my_shelf:
+        globals()[key] = my_shelf[key]
+    my_shelf.close()
+
+    # SOLUTION HERE
+    count = 0
+    buffer = []
+
+    for ch in data[0]:
+        count += 1
+        buffer.append(ch)
+        if len(buffer) == len(set(buffer)) and len(buffer) == 14:
+            return count
+        if len(buffer) == 14:
+            buffer = buffer[1:]
+
+
+def main():
+    day = 6
+    data = parse_input(day)
+    if data:
+        print("Got data successfully")
+    else:
+        print("Error getting data")
+    ans1, ans2 = None, None
+    ans1 = solve1([x for x in data])
+    ans2 = solve2([x for x in data])
+    print(f"Answer a: {ans1}")
+    print(f"Answer b: {ans2}")
+
+
+if __name__ == '__main__':
+    main()
